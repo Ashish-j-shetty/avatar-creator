4185c1124cf7aa047c5397b6b554ae6370b35329
Ashish J shetty <ashish.s@goodworker.in>
2022-09-20 17:31:24 +0530

    feat:phase 2

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import { defaultClothesColor, defaultSkinColor } from "../../../constants";
+import Charecter from "../Charecter";
+import MaxWidthWrapper from "../MaxWidthWrapper/MaxWidthWrapper";
+import styles from "./CharacterEditor.module.css";
 export default function CharacterEditor() {
   const [body, setBody] = React.useState(0);
   const [head, setHead] = React.useState(0);
@@ -8,5 +11,30 @@ export default function CharacterEditor() {
   const [skinColor, setSkinColor] = React.useState(defaultSkinColor);
   const [clothesColor, setClothesColor] = React.useState(defaultClothesColor);
 
-  return <div>CharacterEditor</div>;
+  return (
+    <main className={styles.characterEditor}>
+      <div className={styles.perspectiveEffect}></div>
+
+      <MaxWidthWrapper className={styles.maxWidthWrapper}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>Create your Avatar</h1>
+          <p className={styles.description}>
+            Customize your character's look and style using the controls below.
+            What sort of adventure will you embark on?{" "}
+          </p>
+        </header>
+        <div className={styles.controlColumn}></div>
+      </MaxWidthWrapper>
+      <div className={styles.characterWrapper}>
+        <Charecter
+          body={body}
+          head={head}
+          face={face}
+          accessory={accessory}
+          skinColor={skinColor}
+          clothesColor={clothesColor}
+        />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/MaxWidthWrapper/MaxWidthWrapper.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./MaxWidthWrapper.module.css";
+
+export default function MaxWidthWrapper({
+  children,
+  className = "",
+  ...delegated
+}) {
+  return (
+    <div className={`${styles.maxWidthWrapper}`} {...delegated}>
+      {children}
+    </div>
+  );
+}
