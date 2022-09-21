5031b4ab33c499f376f3ba5089a336ddd02e26ab
Ashish J shetty <ashish.s@goodworker.in>
2022-09-21 08:57:55 +0530

    final : changes

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { defaultClothesColor, defaultSkinColor } from "../../../constants";
-import Charecter from "../Charecter";
-import MaxWidthWrapper from "../MaxWidthWrapper/MaxWidthWrapper";
+
+import { defaultSkinColor, defaultClothesColor } from "../../constants";
+import Character from "../Charecter";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import ControlPane from "../ControlPane";
+
+import {
+  bodyOptions,
+  headOptions,
+  faceOptions,
+  accessoryOptions,
+  skinColorOptions,
+  clothesColorOptions,
+} from "./CharacterEditor.helper";
 import styles from "./CharacterEditor.module.css";
-export default function CharacterEditor() {
+
+function App() {
   const [body, setBody] = React.useState(0);
   const [head, setHead] = React.useState(0);
   const [face, setFace] = React.useState(0);
@@ -14,19 +26,57 @@ export default function CharacterEditor() {
   return (
     <main className={styles.characterEditor}>
       <div className={styles.perspectiveEffect}></div>
-
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
-          <h1 className={styles.title}>Create your Avatar</h1>
+          <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
             Customize your character's look and style using the controls below.
             What sort of adventure will you embark on?{" "}
           </p>
         </header>
-        <div className={styles.controlColumn}></div>
+
+        <div className={styles.controlColumn}>
+          <ControlPane
+            title="Bodies"
+            options={bodyOptions}
+            currentOption={body}
+            handleSelectOption={setBody}
+          />
+          <ControlPane
+            title="Heads"
+            options={headOptions}
+            currentOption={head}
+            handleSelectOption={setHead}
+          />
+          <ControlPane
+            title="Faces"
+            options={faceOptions}
+            currentOption={face}
+            handleSelectOption={setFace}
+          />
+          <ControlPane
+            title="Accessories"
+            options={accessoryOptions}
+            currentOption={accessory}
+            handleSelectOption={setAccessory}
+          />
+          <ControlPane
+            title="Skin Color"
+            options={skinColorOptions}
+            currentOption={skinColor}
+            handleSelectOption={setSkinColor}
+          />
+          <ControlPane
+            title="Clothing Color"
+            options={clothesColorOptions}
+            currentOption={clothesColor}
+            handleSelectOption={setClothesColor}
+          />
+        </div>
       </MaxWidthWrapper>
+
       <div className={styles.characterWrapper}>
-        <Charecter
+        <Character
           body={body}
           head={head}
           face={face}
@@ -38,3 +88,5 @@ export default function CharacterEditor() {
     </main>
   );
 }
+
+export default App;
